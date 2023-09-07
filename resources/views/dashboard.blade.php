72645e03513004ae9a2d7d217270abb14ee3d26f
Maulanazahronyyy <maulanazahronyyy@gmail.com>
2023-09-07 14:48:12 +0700

    Kasih copyright dlu hehe

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -227,6 +227,7 @@
           </div>
         </div>
         <!-- Page body -->
+		
         <div class="page-body">
           <div class="container-xl">
        @yield('siswa')
@@ -234,7 +235,20 @@
           <div class="container-xl">
             <div class="row text-center align-items-center flex-row-reverse">
               <div class="col-lg-auto ms-lg-auto">
-            
+			  <li class="list-inline-item">
+                    <a href="https://github.com/Maulanazahronyyy" target="_blank" class="link-secondary" rel="noopener">
+                      <!-- Download SVG icon from http://tabler-icons.io/i/heart -->
+                      <svg xmlns="http://www.w3.org/2000/svg" class="icon text-pink icon-filled icon-inline" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
+                      Maulana zahrony
+                    </a>
+                  </li>
+              </div>
+			  <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+                <ul class="list-inline list-inline-dots mb-0">
+                  <li class="list-inline-item">
+                    Copyright Maulanazahronyyy 2023	
+                  </li>
+                </ul>
               </div>
               
             </div>
